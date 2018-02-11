e53733be62d8c29d176d66f4d844785250242e84
BigBrosh <30599468+BigBrosh@users.noreply.github.com>
2018-02-11 13:43:35 +0200

    canvas_wrap defining modified

--- project/scripts/Canvas.js
+++ project/scripts/Canvas.js
@@ -2,10 +2,10 @@ import {FrameHandler} from './FrameHandler.js'
 import {Drawing} from './drawLine.js'
 import {UIController} from './UIController.js'
 
-var Canvas = function (canvasWrap, wrap) {	
+var Canvas = function (wrap) {	
 	let self = this;
 
-	self._canvasWrap = document.querySelector(canvasWrap);
+	self._canvasWrap = document.querySelector(wrap + ' .canvas_wrap');
 	self._element = self._canvasWrap.getContext('2d');
 
 	self._frameHandler = new FrameHandler(self._canvasWrap, self._element, wrap);
@@ -21,6 +21,6 @@ var Canvas = function (canvasWrap, wrap) {
 	}
 }
 
-var canvas1 = new Canvas('.canvas_wrap.first', '.main_wrapp.first');
-var canvas2 = new Canvas('.canvas_wrap.second', '.main_wrapp.second');
-var canvas3 = new Canvas('.canvas_wrap.third', '.main_wrapp.third');
+var canvas1 = new Canvas('.main_wrapp.first');
+var canvas2 = new Canvas('.main_wrapp.second');
+var canvas3 = new Canvas('.main_wrapp.third');
