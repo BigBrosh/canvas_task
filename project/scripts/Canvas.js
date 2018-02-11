a46c8f440b7d65f54298fed6a3bfd83c2e1a210a
BigBrosh <30599468+BigBrosh@users.noreply.github.com>
2018-02-11 12:01:56 +0200

    added two painting wraps

--- project/scripts/Canvas.js
+++ project/scripts/Canvas.js
@@ -21,4 +21,6 @@ var Canvas = function (canvasWrap, wrap) {
 	}
 }
 
-var canvas1 = new Canvas('.canvas_wrap.first', '.main_wrapp');
+var canvas1 = new Canvas('.canvas_wrap.first', '.main_wrapp.first');
+var canvas2 = new Canvas('.canvas_wrap.second', '.main_wrapp.second');
+var canvas3 = new Canvas('.canvas_wrap.third', '.main_wrapp.third');
